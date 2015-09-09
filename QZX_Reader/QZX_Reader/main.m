7404244635188382dd57ba24efd0b458fbe12e06
zhangsiyao <a505017520@163.com>
2015-09-09 16:06:29 +0800

    First Test Push
    Updata MainStoryBoard Push Test

---
+++ QZX_Reader/QZX_Reader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QZX_Reader
+//
+//  Created by zhangsiyao on 15/9/9.
+//  Copyright (c) 2015年 ZhangSiYao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
